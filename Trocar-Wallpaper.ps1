54c36a58339da2d42996cad94b438df84e0644cf
mgsolution-jau <gerotti@gmail.com>
2026-09-10 15:40:10 -0300

    Update Trocar-Wallpaper.ps1

--- Trocar-Wallpaper.ps1
+++ Trocar-Wallpaper.ps1
@@ -4,7 +4,7 @@
 
 # 1. Configurações da Logo e Visual
 $CaminhoLogo = "C:\Scripts\logo.png"
-$Opacidade = 0.65       
+$Opacidade = 0.85       
 $TamanhoLogo = 0.06     
 
 # 2. Obtém a resolução real do monitor
